026f45ed609c15201082616913df3b336c8a3d85
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-10-26 23:19:10 +0900

    デバックログ追加

--- app/src/main/java/jp/sample/tsutou/githubClient/view/ui/ProjectListFragment.kt
+++ app/src/main/java/jp/sample/tsutou/githubClient/view/ui/ProjectListFragment.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -51,6 +52,7 @@ class ProjectListFragment : Fragment() {
                 binding.isLoading = false
                 projectAdapter.setProjectList(it)
             }
+            Log.d("ConfirmProjects", projects.toString())
         })
     }
 }
